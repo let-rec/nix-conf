a38ea5f84c79be0c785fecf31bbec18dc2bc3ed8
let-rec <toxtayevhamidulloh6997@gmail.com>
2025-06-16 01:05:34 +0500

    add: shell.nix + fix: flake.nix

--- flake.nix
+++ flake.nix
@@ -2,23 +2,38 @@
   inputs = { 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils"; 
   };
   outputs = {
     self,
     nixpkgs,
     home-manager,
+    flake-utils,
     ...
-  }:
+  } @ inputs:
+
     let 
       lib = nixpkgs.lib;
-    in { 
-      nixosConfigurations = {
-        nixos = lib.nixosSystem {
+      outputs = self;
+    in 
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in 
+        {
+          devShells.default = import ./shell.nix {inherit pkgs;};
+        }
+      )
+      // 
+      {
+        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [ ./configuration.nix ];
         };
-      };
-    };
+      }
 }
---
+++ shell.nix
@@ -0,0 +1,24 @@
+{
+  pkgs ? let 
+   lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+   nixpkgs = fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+    sha256 = lock.narHash;
+   }; 
+  in 
+    import nixpkgs {overlays = [];},
+  ...
+}:
+  pkgs.stdenv.mkDerivation {
+    name = "nix";
+    nativeBuildInputs = with pkgs; [
+      nixd
+      statix
+      deadnix
+      alejandra
+      git
+      #just
+      #sops
+    ];
+    
+  }
