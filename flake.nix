b90ecba5fb97b284f2efa57824c3059348366585
let-rec <toxtayevhamidulloh6997@gmail.com>
2025-06-16 15:46:42 +0500

    fix: flake.nix

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
       {
         nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          modules = [ ./configuration.nix ];
+          modules = [ ./nixos/configuration.nix ];
         };
-      }
+      };
 }
