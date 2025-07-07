95eaa789251e1e134d934e69e477a71356e6f14a
let-rec <toxtayevhamidulloh6997@gmail.com>
2025-07-07 16:51:24 +0500

    feat: ssh pass

--- modules/nixos/ssh/default.nix
+++ modules/nixos/ssh/default.nix
@@ -8,7 +8,7 @@
         # Forbid root login through SSH.
         PermitRootLogin = "no";
         # Use keys only. Remove if you want to SSH using password (not recommended)
-        PasswordAuthentication = false;
+        PasswordAuthentication = true;
       };
     };
   };
