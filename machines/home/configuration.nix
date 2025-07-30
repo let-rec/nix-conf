e38463ce0c57c76efde6ff5610a3aa4f2e0d3d39
let-rec <toxtayevhamidulloh6997@gmail.com>
2025-07-30 22:49:45 +0500

    fix: hardware graphics problem

--- machines/home/configuration.nix
+++ machines/home/configuration.nix
@@ -90,14 +90,10 @@
 
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
   };
 
   hardware = {
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-    };
     nvidia = {
       open = false;
       modesetting.enable = true;
