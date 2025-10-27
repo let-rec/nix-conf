c2e98b5c118e93d83c5c73b4ae9d7be35e8400d0
rec <toxtayevhamidulloh6997@gmail.com>
2025-10-27 17:59:55 +0500

    chore: added gpg signing key

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -3,5 +3,19 @@
     enable = true;
     lfs.enable = true;
     difftastic.enable = true;
+
+    userName = "let-rec";
+    userEmail = "toxtayevhamidulloh6997@gmail.com";
+
+    signing = {
+      signByDefault = true;
+      key = "06F6FCE379C490BAC47F4ACEE9180B76B6DB2E66";
+    };
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      http.sslVerify = false;
+      pull.rebase = false;
+    };
   };
 }
